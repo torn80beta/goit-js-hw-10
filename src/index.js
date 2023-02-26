62b5037a25989915df49eb02c22db61a0eb6d3bc
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-26 17:16:34 +0200

    fetch flags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
-import fetchCountries from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
+//import fetchCountries from './js/fetchCountries';
 var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
@@ -9,9 +10,36 @@ const refs = {
   countryInfoEl: document.querySelector('.country-info'),
 };
 
-const country = refs.searchBoxEl.value;
-console.log(refs.searchBoxEl, refs.countryListEl, refs.countryInfoEl);
-refs.searchBoxEl.addEventListener(
-  'input',
-  debounce(fetchCountries, DEBOUNCE_DELAY)
-);
+let searchQuery = '';
+
+refs.searchBoxEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  searchQuery = e.target.value;
+  fetchCountries(searchQuery).then(data => showFlag(data));
+}
+
+function showFlag(countries) {
+  refs.countryListEl.innerHTML = '';
+  const markup = countries
+    .map(
+      country =>
+        `<li class="country"><img src='${country.flags.svg}' width=30 height=20 alt="${country.flags.alt}"><p class="country-name">${country.name.common}</p></li>`
+    )
+    .join('');
+  refs.countryListEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+// function fetchCountries(e) {
+//   fetch(
+//     `https://restcountries.com/v3.1/name/${e.target.value}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(res => {
+//       //console.log(res.json());
+//       return res.json();
+//     })
+//     .then(res => {
+//       showFlag(res);
+//       console.log(res[0].name);
+//     });
+// }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,13 @@
-export default function fetchCountries(e) {
-  console.log(e.target.value);
+export function fetchCountries(e) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`
+  )
+    .then(res => {
+      //console.log(res.json());
+      return res.json();
+    })
+    .then(data => {
+      //console.log(data);
+      return data;
+    });
 }
